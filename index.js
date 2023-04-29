c9972815fa3c0c117cf46030a0003f01beaccf79
Aluno talentos <rafael.rodrigues022@hotmail.com>
2023-04-28 21:02:46 -0300

    exercicios

---
+++ index.js
@@ -0,0 +1,65 @@
+// const name = prompt("Nome?:")
+// console.log(`o "${nome} é turista`)
+// const nome = "maria"
+// console.log("o nome é",nome);
+// // Exercício 1
+// const nome = prompt("Nome?")
+// const cor = prompt("digite sua cor?");
+// console.log("a cor favorita" + nome + "é" + cor);
+
+// const nome = "jose"
+// console.log(nome.length);
+
+// const nome = prompt("Digite seu nome")
+// // Verificar se a pessoa escreveu algo
+// nome.length === 0 ? 
+// console.log("voce precisa digitar algo"):
+// console.log("o nome é", nome);
+
+// const sobrenome = "Marques"
+// console.log(sobrenome.toLowerCase())
+// console.log(sobrenome.toUpperCase())
+
+// const nome = "    rafaela"
+// console.log(nome.trim())
+
+
+// const receita = "bolo de cenoura"
+// console.log(receita.includes("cenoura"));
+
+// const receita = "bolo de abacaxi"
+// const novaReceita = receita.replace("abacaxi","cenoura")
+
+// Exercício 2
+// const frase = prompt("escreva uma frase")
+//const fraseMaiuscula = frase.toUpperCase()
+//const novaFrase = fraseMaiuscula.replaceAll("o","i")
+//console.log(novaFrase);
+//console.log(novaFrase.lenght);
+
+// const numeros = [14, 242, 673, 534, 5325];
+//console.log(numeros);
+
+// Exercício 3 
+//const raças = ["poodle", "pitbull", "maltês", "labrador", "pintcher"];
+// cachorros.splice(1,1)
+// console.log(cachorros.lenght);
+//const numero = Number(prompt("digite um número entre 0 a 4"));
+
+//console.log(cachorros[0]);
+// cachorros[numero] === underfined ?
+// console.log("Você deve informar numeros entre 0 e 4") :
+// console.log(cachorros[numero]);
+// console.log(cachorros.includes("caramelo"));
+// cachorros.push("outro cachorro qualquer")
+// console.log(cachorros);
+
+// const racaCachorro = prompt("raça de cachorro");
+// cachorros.push(racaCachorro);
+// console.log(cachorros);
+// cachorros.pop()
+
+// const numeros = ["1", "2", "3", "4", "5", "6"]
+
+
+
